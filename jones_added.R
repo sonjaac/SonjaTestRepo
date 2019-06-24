ce0dd3c1084fea61be955f2eb7263eae96f6a327
Sonja <chris625@msu.edu>
2019-06-24 16:05:32 -0400

    Sonja edited the first three lines

--- jones_added.R
+++ jones_added.R
@@ -1,2 +1,3 @@
-#This is mike adding a script to Sonja's project again
-y <- x/5
+##Sonja added this line
+#This is mike adding a script to Sonja's project again ## Here's sonja's change
+y <- (x/5)^2
